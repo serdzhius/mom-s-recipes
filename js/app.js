d394120f85994202d5a9c5e7c89f84e3bfd9a4a3
serdzhius <105393692+serdzhius@users.noreply.github.com>
2023-11-24 21:43:45 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -421,6 +421,11 @@
         }));
         document.querySelector("button").addEventListener("click", (function() {}));
     }));
+    window.addEventListener("DOMContentLoaded", (function() {
+        const scrollPosition = window.scrollY;
+        window.location.hash = "";
+        window.scrollTo(0, scrollPosition);
+    }), false);
     window["FLS"] = true;
     isWebp();
     spollers();
